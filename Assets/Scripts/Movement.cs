be6ed9e09bbab7dcd5764428bd6e4d4d1553b3f2
nvclas <74192845+nvclas@users.noreply.github.com>
2023-09-22 17:52:28 +0200

    Jump recode (noch nicht fertig)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,60 +1,73 @@
 using System.Collections;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
-{
-    public float speed = 10f;
-    public float jumpHeight = 20f;
-    public float baseFallMultiplier = 5f;
-    public float terminalVelocity = 5f;
-    public float fallAcceleration = 3f;
+public class Movement : MonoBehaviour {
+    public float baseJumpForce = 10f; // Basis Sprungstärke
+    public float baseMoveSpeed = 10f; // Basis Bewegungsgeschwindigkeit
+    public float maxJumpTime = 0.5f; // Maximale Sprungzeit
+    public float fallAcceleration = 3f; // Fallbeschleunigung
+    public float maxFallVelocity = 10f; // Maximale Fallgeschwindigkeit
 
-    private Rigidbody2D _rb;
-    private float _fallMultiplier;
+    private float _fallVelocity; // Aktuelle Fallgeschwindigkeit
+    private bool _onGround; // Bodenkontakt
+    private float _jumpTime; // Sprungzeitpunkt
 
-    // Start is called before the first frame update
-    private void Start()
-    {
-        _rb = GetComponent<Rigidbody2D>();
-        _fallMultiplier = baseFallMultiplier;
+    private Rigidbody2D _rigidbody2D; // Physikkörper des Spielers
+
+    // Initialisierung
+    private void Start() {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    private void Update()
-    {
-        _rb.velocity = new Vector2(speed, _rb.velocity.y);
+    // Fixed Update nach festem Zeitintervall aufgerufen
+    private void Update() {
+        // Vorwärtsbewegung
+        _rigidbody2D.velocity = new Vector2(baseMoveSpeed, _rigidbody2D.velocity.y);
+
         Jump();
-
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            transform.position = new Vector2(0, 0);
-        }
-    }
-
-    private void Jump()
-    {
-        if (Input.GetButtonDown("Jump"))
-        {
-            _rb.velocity = new Vector2(_rb.velocity.x, 0);
-            _rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
-        }
-        else if (_rb.velocity.y < 0)
-        {
-            _rb.velocity += Vector2.up * (Physics.gravity.y * _fallMultiplier * Time.deltaTime);
-            if (_rb.velocity.y > terminalVelocity)
-            {
-                StartCoroutine(FallFaster());
+        
+        if (_rigidbody2D.velocity.y < 0) {
+            // Standardfallgeschwindigkeit erhöhen
+            _rigidbody2D.velocity += Vector2.up * (Physics.gravity.y * _fallVelocity * Time.deltaTime);
+            if (_rigidbody2D.velocity.y > maxFallVelocity) {
+                //Fallgeschwindigkeit weiter beschleunigen, wenn noch nicht maximal
+                StartCoroutine(Fall());
             }
         }
-        else if (_rb.velocity.y >= 0)
-        {
-            _fallMultiplier = baseFallMultiplier;
+    }
+
+    private void Jump() {
+        // Überprüfe, ob Spieler auf Boden ist und auf Bildschirm getippt wird
+        // "Fire1" = Linksklick oder Bildschirmtippen
+        if (_onGround && Input.GetButtonDown("Fire1")) {
+            // Sprungzeitpunkt speichern
+            _jumpTime = Time.time;
+
+            // Kraft nach oben, um Spieler springen zu lassen
+            _rigidbody2D.AddForce(Vector2.up * baseJumpForce, ForceMode2D.Impulse);
+            // Spieler jetzt in Luft
+            _onGround = false;
+            return;
+        }
+
+        if (!_onGround && (Input.GetButtonUp("Fire1") || Time.time - _jumpTime >= maxJumpTime)) {
+            // Sprungzeit zurücksetzen
+            _jumpTime = 0;
         }
     }
 
-    IEnumerator FallFaster()
-    {
+    // Aufgerufen, wenn Objekt mit anderem Collider kollidiert
+    private void OnCollisionEnter2D(Collision2D collision) {
+        // Überprüfe, ob Spieler mit dem Boden kollidiert
+        if (collision.gameObject.CompareTag("Ground")) {
+            // Spieler wieder auf dem Boden
+            _onGround = true;
+        }
+    }
+
+    private IEnumerator Fall() {
         yield return new WaitForSeconds(0.1f);
-        _fallMultiplier += fallAcceleration;
+        // Alle 0.1 Sekunden beschleunigen
+        _fallVelocity += fallAcceleration;
     }
 }
