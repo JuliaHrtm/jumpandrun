ec57c5f438c8eaa3ffa745b70202ee35724089d2
nvclas <74192845+nvclas@users.noreply.github.com>
2023-09-24 18:19:16 +0200

    Sprung fertig, Coins eingefügt (noch nicht einsammelbar), Coins animiert, Coin-Sprite etwas abgeändert

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,15 +1,14 @@
-using System.Collections;
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
-    public float baseJumpForce = 10f; // Basis Sprungstärke
+    public float baseJumpForce = 1f; // Basis Sprungstärke
     public float baseMoveSpeed = 10f; // Basis Bewegungsgeschwindigkeit
-    public float maxJumpTime = 0.5f; // Maximale Sprungzeit
+    public float maxJumpTime = 0.3f; // Maximale Sprungzeit
+    public float minJumpTime = 0.1f; // Minmale Sprungzeit
     public float fallAcceleration = 3f; // Fallbeschleunigung
-    public float maxFallVelocity = 10f; // Maximale Fallgeschwindigkeit
 
-    private float _fallVelocity; // Aktuelle Fallgeschwindigkeit
     private bool _onGround; // Bodenkontakt
+    private bool _isJumping; // Springt
     private float _jumpTime; // Sprungzeitpunkt
 
     private Rigidbody2D _rigidbody2D; // Physikkörper des Spielers
@@ -17,57 +16,63 @@ public class Movement : MonoBehaviour {
     // Initialisierung
     private void Start() {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _rigidbody2D.gravityScale = fallAcceleration;
     }
 
-    // Fixed Update nach festem Zeitintervall aufgerufen
+    // Update jeden Frame aufgerufen (genutzt für Steuerung)
     private void Update() {
         // Vorwärtsbewegung
         _rigidbody2D.velocity = new Vector2(baseMoveSpeed, _rigidbody2D.velocity.y);
 
-        Jump();
-        
-        if (_rigidbody2D.velocity.y < 0) {
-            // Standardfallgeschwindigkeit erhöhen
-            _rigidbody2D.velocity += Vector2.up * (Physics.gravity.y * _fallVelocity * Time.deltaTime);
-            if (_rigidbody2D.velocity.y > maxFallVelocity) {
-                //Fallgeschwindigkeit weiter beschleunigen, wenn noch nicht maximal
-                StartCoroutine(Fall());
+        if (Input.GetButton("Fire1") && _onGround) // Springen, wenn auf Boden und Bildschirmtipp
+            StartJump();
+        if (!Input.GetButton("Fire1") && _jumpTime > minJumpTime) // Aufhören zu springen, wenn Bildschirm zu lang gehalten oder losgelassen
+            EndJump();
+    }
+
+    // Aufruf in festen Zeitintervallen (genutzt für Physik)
+    private void FixedUpdate() {
+        // Spieler springt
+        if (_isJumping) {
+            // Aktuelle Sprungdauer erhöhen
+            _jumpTime += Time.fixedDeltaTime;
+            // Maximale Sprungdauer bereits überschritten?
+            if (_jumpTime < maxJumpTime) {
+                // Spieler noch höher springen lassen
+                _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, baseJumpForce);
             }
         }
     }
 
-    private void Jump() {
-        // Überprüfe, ob Spieler auf Boden ist und auf Bildschirm getippt wird
-        // "Fire1" = Linksklick oder Bildschirmtippen
-        if (_onGround && Input.GetButtonDown("Fire1")) {
-            // Sprungzeitpunkt speichern
-            _jumpTime = Time.time;
-
-            // Kraft nach oben, um Spieler springen zu lassen
-            _rigidbody2D.AddForce(Vector2.up * baseJumpForce, ForceMode2D.Impulse);
-            // Spieler jetzt in Luft
-            _onGround = false;
-            return;
+    private void StartJump() {
+        if (!_isJumping) {
+            _isJumping = true; // Spieler springt
+            _jumpTime = 0f;
         }
+    }
 
-        if (!_onGround && (Input.GetButtonUp("Fire1") || Time.time - _jumpTime >= maxJumpTime)) {
-            // Sprungzeit zurücksetzen
+    private void EndJump() {
+        if (_isJumping) {
+            _isJumping = false; // Spieler springt nicht mehr
             _jumpTime = 0;
         }
     }
 
     // Aufgerufen, wenn Objekt mit anderem Collider kollidiert
-    private void OnCollisionEnter2D(Collision2D collision) {
-        // Überprüfe, ob Spieler mit dem Boden kollidiert
-        if (collision.gameObject.CompareTag("Ground")) {
-            // Spieler wieder auf dem Boden
+    private void OnCollisionEnter2D(Collision2D other) {
+        // Kolldiiert Spieler mit Boden?
+        if (other.gameObject.CompareTag("Ground")) {
+            // Spieler auf Boden
             _onGround = true;
         }
     }
 
-    private IEnumerator Fall() {
-        yield return new WaitForSeconds(0.1f);
-        // Alle 0.1 Sekunden beschleunigen
-        _fallVelocity += fallAcceleration;
+    // Aufgerufen, wenn Objekt anderen Collider nicht mehr berührt
+    private void OnCollisionExit2D(Collision2D other) {
+        // Verlässt Spieler Boden?
+        if (other.gameObject.CompareTag("Ground")) {
+            // Spieler in der Luft
+            _onGround = false;
+        }
     }
 }
