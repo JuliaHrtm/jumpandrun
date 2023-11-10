3c3c057d6c0179b2de9f5a33931ec48c0058bdf5
nvclas <74192845+nvclas@users.noreply.github.com>
2023-11-10 17:47:25 +0100

    Revert "Merge branch 'niclas' into dev"
    This reverts commit d2d142b0c0e3539b3339f19e79fc70c5d18d7e14, reversing
    changes made to b3dc53c7d5a29a1010ca98e0ec77a3bb654f3f81.

--- Assets/Scripts/CoinCounter.cs
+++ Assets/Scripts/CoinCounter.cs
@@ -5,18 +5,18 @@ public class CoinCounter : MonoBehaviour {
     public static CoinCounter Instance;
 
     public TMP_Text coinText;
-    public int currentCoin;
+    public int currentCoin = 0;
 
     private void Awake() {
         Instance = this;
     }
 
     private void Start() {
-        coinText.text = "Score: " + currentCoin;
+        coinText.text = "Score: " + currentCoin.ToString();
     }
 
     public void IncreaseCoin(int v) {
         currentCoin += v;
-        coinText.text = "Score: " + currentCoin;
+        coinText.text = "Score: " + currentCoin.ToString();
     }
 }
